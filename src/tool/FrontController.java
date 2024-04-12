50d896313aaf55333426f87e3179c3199ef67ef9
oohara <oohara@172.16.31.71>
2024-04-12 11:39:14 +0900

    examプロジェクトの新規登録

---
+++ src/tool/FrontController.java
@@ -0,0 +1,27 @@
+package tool;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FrontController")
+public class FrontController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: "); append(request.getContextPath());
+		
+	}
+	private void append(String contextPath) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		doGet(request, response);
+	}
+}
